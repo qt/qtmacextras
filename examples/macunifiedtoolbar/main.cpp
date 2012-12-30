e827033d80f534caffae13e2b30b13e8349365bd
Jake Petroules <jake.petroules@petroules.com>
2012-12-29 22:48:27 -0500

    Add more functionality to QtMacUnifiedToolBar.
    Getters and setters are now provided for:
    * Toolbar visibility
    * Baseline separator visibility
    * Whether to allow user customization menu
    * Button style (icon only, text and icon, etc.)
    * Icon size (small or regular)
    
    Added QSize getters and setters for the iconSize property for
    compatibility with the QToolBar API.
    
    Added signals to react to changes to any of these properties, which
    will also be emitted when the properties are changed through
    native Cocoa code, not just the QtMacUnifiedToolbar API. This
    allows the application to respond to the user right clicking
    the toolbar and changing button style, visibility, etc., from
    there.
    
    The example app now includes a bunch of controls to manipulate
    the toolbar's properties so user's can visually explore the
    capabilities of the API.
    
    Change-Id: I2fd8b9d84fd4f981675eccc48f275e11524c6875

--- examples/macunifiedtoolbar/main.cpp
+++ examples/macunifiedtoolbar/main.cpp
@@ -39,31 +39,14 @@
  **
  ****************************************************************************/
 
-#include "qtmacunifiedtoolbar.h"
 #include <QApplication>
-#include <QWidget>
+#include "window.h"
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
-
-    QWidget widget;
-    widget.resize(300, 300);
-    widget.setWindowTitle("Qt Mac Toolbar Example");
-
-    QtMacUnifiedToolBar toolBar;
-    toolBar.addAction(QIcon(":/qtlogo.png"), "Hello");
-    toolBar.addAction(QIcon(":/qtlogo.png"), "World");
-    toolBar.addStandardItem(QtMacToolButton::FlexibleSpace);
-    toolBar.addStandardItem(QtMacToolButton::ShowColors);
-    toolBar.addStandardItem(QtMacToolButton::ShowFonts);
-
-    toolBar.addAllowedAction(QIcon(":/qtlogo.png"), "Extra Button 1");
-    toolBar.addAllowedAction(QIcon(":/qtlogo.png"), "Extra Button 2");
-
-    toolBar.showInWindowForWidget(&widget);
-    widget.show();
-
+    Window window;
+    window.show();
     return app.exec();
 }
 
---
+++ examples/macunifiedtoolbar/window.h
@@ -0,0 +1,34 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <QWidget>
+#include "qtmactoolbutton.h"
+
+namespace Ui {
+class Window;
+}
+
+class WindowPrivate;
+class Window : public QWidget
+{
+    Q_OBJECT
+    
+public:
+    explicit Window(QWidget *parent = 0);
+    ~Window();
+
+private slots:
+    void changeDisplayMode(int);
+    void displayModeChanged(Qt::ToolButtonStyle);
+
+    void changeSizeMode(int);
+    void sizeModeChanged(QtMacToolButton::IconSize);
+
+    void positionWindow();
+    
+private:
+    WindowPrivate *d;
+    Ui::Window *ui;
+};
+
+#endif // WINDOW_H
